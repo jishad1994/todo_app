a7f02a51d7bc9a3098a5f7e74335b2852651f3c8
jishad kolapurath <jishadkolapurath@gmail.com>
2025-04-03 17:16:56 +0530

    for updation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,25 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo_app';
+  public count: number = 0;
+
+  oddOrEven: string = 'even';
+  ngOnInit() {
+    console.log('app compoonetn initialized');
+  }
+
+  setColor(): string {
+    return 'red';
+  }
+
+  increaseCount(): void {
+    this.count++;
+  }
+  decreaseCount(): void {
+    this.count--;
+  }
 }
