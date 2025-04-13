d0da7a5e591490c9ee0300bdcab548f26dea94cb
jishad kolapurath <jishadkolapurath@gmail.com>
2025-04-13 17:38:28 +0530

    cleared version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    NgFor,
+    
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'todo_app';
-  public count: number = 0;
-
-  oddOrEven: string = 'even';
-  ngOnInit() {
-    console.log('app compoonetn initialized');
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
   }
 
-  setColor(): string {
-    return 'red';
-  }
-
-  increaseCount(): void {
-    this.count++;
-  }
-  decreaseCount(): void {
-    this.count--;
-  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import{withForms} from '@angular/forms';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),withForms()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
-
-
